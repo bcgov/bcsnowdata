238e4d608f3a06c00804194352265c4fce1dd39a
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2021-07-22 10:06:41 -0700

    Style changes

--- R/snow_manuallocation_function.R
+++ R/snow_manuallocation_function.R
@@ -1,4 +1,4 @@
-# Copyright 2019 Province of British Columbia
+# Copyright 2021 Province of British Columbia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -9,19 +9,14 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+# ==============
 
-
-#' Get the location of manual snow sites
-#'
-#' This function gets the location of snow surveys from the Data BC catalogue
+#' This function gets the location of snow surveys from the Data BC catalog
 #' @param crs Defaults to 3005
 #' @keywords Get manual snow survey locations
 #' @export
 #' @examples
 #' snow_manual_location()
-
 snow_manual_location <- function(crs = "3005") {
-
   databc_GET_client("WHSE_WATER_MANAGEMENT.SSL_SNOW_MSS_LOCS_SP", crs = crs)
-
 }
