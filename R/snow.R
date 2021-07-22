4477c1dfc8f228831843d8b8fab145357c5426e5
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2021-07-22 10:06:22 -0700

    Style changes

--- R/snow.R
+++ R/snow.R
@@ -1,4 +1,4 @@
-# Copyright 2019 Province of British Columbia
+# Copyright 2021 Province of British Columbia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,19 +16,20 @@
 
 # Get historic data - pre 2011 daily data
 snow_daily_archive_ <- function() {
-
   message("Reading Archived Daily Snow Data")
   res <- GET_client(link = "https://pub.data.gov.bc.ca/datasets/5e7acd31-b242-4f09-8a64-000af872d68f/daily_asp_archive.csv")
 
-  archive <- httr::content(res, encoding = "UTF-8", type = "text/csv",
-                           col_types = readr::cols(
-                             Pillow_ID = readr::col_character(),
-                             Date = readr::col_character(),
-                             variable = readr::col_character(),
-                             value = readr::col_double(),
-                             code = readr::col_character()
-                           ),
-                           progress = FALSE)
+  archive <- httr::content(res,
+    encoding = "UTF-8", type = "text/csv",
+    col_types = readr::cols(
+      Pillow_ID = readr::col_character(),
+      Date = readr::col_character(),
+      variable = readr::col_character(),
+      value = readr::col_double(),
+      code = readr::col_character()
+    ),
+    progress = FALSE
+  )
 
   ## Trouble parsing because of all NA. Remove NA first then parse.
   archive <- archive[!is.na(archive$value), ]
@@ -42,12 +43,10 @@ snow_daily_archive_ <- function() {
   names(archive)[names(archive) == "Pillow_ID"] <- "Station_ID"
 
   archive[, c("Date_UTC", "Station_ID", "value", "code", "variable")]
-
 }
 
 # Get the data from 2011 to the present year. This is the hourly data
 snow_auto_archive_ <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitation", "Temperature")) {
-
   parameter_id <- match.arg(parameter_id)
 
   if (parameter_id == "SWE") {
@@ -75,25 +74,28 @@ snow_auto_archive_ <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitat
   res_archive <- GET_client(link = sprintf("http://www.env.gov.bc.ca/wsd/data_searches/snow/asws/data/%s_Archive.csv", link))
 
 
-  tbl <- httr::content(res_archive, encoding = "UTF-8", type = "text/csv",
-                              col_types = readr::cols(.default = readr::col_double(),
-                                                      `DATE(UTC)` = readr::col_datetime()), progress = FALSE)
+  tbl <- httr::content(res_archive,
+    encoding = "UTF-8", type = "text/csv",
+    col_types = readr::cols(
+      .default = readr::col_double(),
+      `DATE(UTC)` = readr::col_datetime()
+    ), progress = FALSE
+  )
 
   long <- tidyr::gather(tbl, site, value, -`DATE(UTC)`)
 
-  long$Station_ID <- substr(long$site, 1,5)
+  long$Station_ID <- substr(long$site, 1, 5)
 
   long$station_name <- substr(long$site, 7, nchar(long$site))
   long$Date_UTC <- long$`DATE(UTC)`
 
   long$variable <- long_name
 
-  long[,c("Date_UTC", "Station_ID", "station_name", "value", "variable")]
+  long[, c("Date_UTC", "Station_ID", "station_name", "value", "variable")]
 }
 
 # Current year snow data
 snow_auto_current_ <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitation", "Temperature")) {
-
   parameter_id <- match.arg(parameter_id)
 
   if (parameter_id == "SWE") {
@@ -120,9 +122,13 @@ snow_auto_current_ <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitat
   res_current <- GET_client(link = sprintf("http://www.env.gov.bc.ca/wsd/data_searches/snow/asws/data/%s.csv", link))
 
 
-  tbl <- httr::content(res_current, encoding = "UTF-8", type = "text/csv",
-                              col_types = readr::cols(.default = readr::col_double(),
-                                                      `DATE(UTC)` = readr::col_datetime()))
+  tbl <- httr::content(res_current,
+    encoding = "UTF-8", type = "text/csv",
+    col_types = readr::cols(
+      .default = readr::col_double(),
+      `DATE(UTC)` = readr::col_datetime()
+    )
+  )
 
   long <- tidyr::gather(tbl, site, value, -`DATE(UTC)`)
 
@@ -138,9 +144,7 @@ snow_auto_current_ <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitat
 
 
 snow_auto_stations <- function(crs = "3005") {
-
   databc_GET_client("WHSE_WATER_MANAGEMENT.SSL_SNOW_ASWS_STNS_SP", crs = crs)
-
 }
 
 # ======================================
@@ -152,38 +156,40 @@ snow_manual_archive_ <- function() {
   res <- GET_client("http://www.env.gov.bc.ca/wsd/data_searches/snow/asws/data/allmss_archive.csv")
 
   # Retrieve data archive from Data BC
-  data_manual <- httr::content(res, encoding = "UTF-8",
-                col_types = readr::cols(
-                  `Snow Course Name` = readr::col_character(),
-                   Number = readr::col_character(),
-                  `Elev. metres` = readr::col_double(),
-                  `Date of Survey` = readr::col_date(format = ""),
-                  `Snow Depth cm` = readr::col_double(),
-                  `Water Equiv. mm` = readr::col_double(),
-                  `Survey Code` = readr::col_character(),
-                  `Snow Line Elev. m` = readr::col_double(),
-                  `Snow Line Code` = readr::col_character(),
-                  `% of Normal` = readr::col_double(),
-                  `Density %` = readr::col_double(),
-                  `Survey Period` = readr::col_character(),
-                  `Normal mm` = readr::col_double()
-                ))
+  data_manual <- httr::content(res,
+    encoding = "UTF-8",
+    col_types = readr::cols(
+      `Snow Course Name` = readr::col_character(),
+      Number = readr::col_character(),
+      `Elev. metres` = readr::col_double(),
+      `Date of Survey` = readr::col_date(format = ""),
+      `Snow Depth cm` = readr::col_double(),
+      `Water Equiv. mm` = readr::col_double(),
+      `Survey Code` = readr::col_character(),
+      `Snow Line Elev. m` = readr::col_double(),
+      `Snow Line Code` = readr::col_character(),
+      `% of Normal` = readr::col_double(),
+      `Density %` = readr::col_double(),
+      `Survey Period` = readr::col_character(),
+      `Normal mm` = readr::col_double()
+    )
+  )
 
- # add in water year to the data
+  # add in water year to the data
   data_manual$wr <- wtr_yr(data_manual$`Date of Survey`)
 
- # Replace station IDs that show up as scientific notation within the archived data
- data_manual_final <- data_manual %>%
-  dplyr::mutate(Number = replace(Number, Number == "2.00E+01", "2E01")) %>%
-  dplyr::mutate(Number = replace(Number, Number == "2.00E+02", "2E02")) %>%
-  dplyr::mutate(Number = replace(Number, Number == "2.00E+03", "2E03")) %>%
-  dplyr::mutate(Number = replace(Number, Number == "1.00E+07", "1E07")) %>%
-  dplyr::mutate(Number = replace(Number, Number == "4.00E+01", "4E01")) %>%
-  dplyr::distinct(Number, `Date of Survey`, `Snow Depth cm`, .keep_all = TRUE)
- }
+  # Replace station IDs that show up as scientific notation within the archived data
+  data_manual_final <- data_manual %>%
+    dplyr::mutate(Number = replace(Number, Number == "2.00E+01", "2E01")) %>%
+    dplyr::mutate(Number = replace(Number, Number == "2.00E+02", "2E02")) %>%
+    dplyr::mutate(Number = replace(Number, Number == "2.00E+03", "2E03")) %>%
+    dplyr::mutate(Number = replace(Number, Number == "1.00E+07", "1E07")) %>%
+    dplyr::mutate(Number = replace(Number, Number == "4.00E+01", "4E01")) %>%
+    dplyr::distinct(Number, `Date of Survey`, `Snow Depth cm`, .keep_all = TRUE)
+}
 
- # Current year manual snow data
- snow_manual_current_ <- function() {
+# Current year manual snow data
+snow_manual_current_ <- function() {
   res <- GET_client("http://www.env.gov.bc.ca/wsd/data_searches/snow/asws/data/allmss_current.csv")
 
   ## TODO - specify parsing to avoid message
@@ -199,9 +205,7 @@ snow_manual_archive_ <- function() {
 
 # Get the location of manual snow stations
 snow_manual_stations <- function(crs = "3005") {
-
   databc_GET_client("WHSE_WATER_MANAGEMENT.SSL_SNOW_MSS_LOCS_SP", crs = crs)
-
 }
 
 # ======================================
@@ -227,10 +231,10 @@ ASWE_data_archive <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitati
 
   # Replace the user input values with the values within the data variable space
   if (parameter_id == "SWE") {
-   archive_var <- gsub("SWE", "Snow_Water_Equivalent", parameter_id)
+    archive_var <- gsub("SWE", "Snow_Water_Equivalent", parameter_id)
   }
   if (parameter_id == "Snow_Depth") {
-   archive_var <- gsub("Snow_Depth", "Snow_Depth", parameter_id)
+    archive_var <- gsub("Snow_Depth", "Snow_Depth", parameter_id)
   }
   if (parameter_id == "Temperature") {
     archive_var <- c("Temp_Max", "Temp_Min")
@@ -248,27 +252,23 @@ ASWE_data_archive <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitati
 
   # filter pre 2011 data to ensure that there is no data overlap between that avialble within the hourly data
   archive_pre2011 <- archive_pre2011 %>%
-    dplyr::filter(Date_UTC < min(archive_2011present$Date_UTC)) #take only data not within the post 2011 hourly data
+    dplyr::filter(Date_UTC < min(archive_2011present$Date_UTC)) # take only data not within the post 2011 hourly data
 
   # Compile together
   archive_all <- dplyr::full_join(archive_pre2011, archive_2011present)
-
 }
 
 # Function for retrieving the current year data and binding to a
 snow_2011tcurrent_archived <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitation", "Temperature"), archived) {
-
   parameter_id <- match.arg(parameter_id)
 
   tbl_current <- snow_auto_current(parameter_id = parameter_id)
 
   dplyr::bind_rows(tbl_current, archived)
-
 }
 
 # Function for binding together the 2011-current year data
 snow_auto <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitation", "Temperature")) {
-
   parameter_id <- match.arg(parameter_id)
 
   tbl_current <- snow_auto_current(parameter_id = parameter_id)
@@ -276,5 +276,4 @@ snow_auto <- function(parameter_id = c("SWE", "Snow_Depth", "Precipitation", "Te
   tbl_archive <- snow_auto_archive(parameter_id = parameter_id)
 
   dplyr::bind_rows(tbl_current, tbl_archive)
-
 }
