5d9e2778bb3ab1a9339dcf26a806282d37714295
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2021-07-22 10:06:46 -0700

    Style changes

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-# Copyright 2019 Province of British Columbia
+# Copyright 2021 Province of British Columbia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -9,8 +9,7 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
-
-
+# =====================
 
 # =====================
 # Function for returning elevation by station
@@ -24,12 +23,12 @@ elevation <- function(data_manual_final, ...) {
     dplyr::rename(Name = Snow_Course_Name) %>%
     dplyr::distinct(Station_ID, .keep_all = TRUE)
 
- auto <- snow_auto_stations() %>%
-   dplyr::rename(Station_ID = LOCATION_ID) %>%
-   dplyr::select(Station_ID, ELEVATION, LOCATION_NAME, geometry) %>%
-   dplyr::rename(Elevation = ELEVATION, Name = LOCATION_NAME)
+  auto <- snow_auto_stations() %>%
+    dplyr::rename(Station_ID = LOCATION_ID) %>%
+    dplyr::select(Station_ID, ELEVATION, LOCATION_NAME, geometry) %>%
+    dplyr::rename(Elevation = ELEVATION, Name = LOCATION_NAME)
 
- stations_el <- dplyr::bind_rows(auto, manual_data)
+  stations_el <- dplyr::bind_rows(auto, manual_data)
 }
 
 # =====================
@@ -39,13 +38,15 @@ basin_name <- function(id, exceptions = NULL) {
   # get all of the sites within the archive
   all_sites <- unique(c(snow_auto_stations()$LOCATION_ID, snow_manual_stations()$LOCATION_ID))
   # Apply exceptions - remove sites that should be removed
-  all_sites <-  all_sites[!all_sites %in% exceptions]
+  all_sites <- all_sites[!all_sites %in% exceptions]
 
   # associate basins by the ID number
-  basins_all <- c("UpperFraserWest", "UpperFraserEast", "Nechako", "MiddleFraser", "LowerFraser", "NorthThompson",
-                  "SouthThompson", "UpperColumbia", "WestKootenay", "EastKootenay", "Okanagan", "Boundary", "Similkameen", "SouthCoast",
-                  "VancouverIsland", "CentralCoast", "Skagit", "Peace", "SkeenaNass", "Stikine", "Liard", "Northwest", "HaidaGwaii",
-                  "Nicola_old", "FraserPlateau", "LillBridge", "Quesnel", "LowerThompson", "Fraser", "Province")
+  basins_all <- c(
+    "UpperFraserWest", "UpperFraserEast", "Nechako", "MiddleFraser", "LowerFraser", "NorthThompson",
+    "SouthThompson", "UpperColumbia", "WestKootenay", "EastKootenay", "Okanagan", "Boundary", "Similkameen", "SouthCoast",
+    "VancouverIsland", "CentralCoast", "Skagit", "Peace", "SkeenaNass", "Stikine", "Liard", "Northwest", "HaidaGwaii",
+    "Nicola_old", "FraserPlateau", "LillBridge", "Quesnel", "LowerThompson", "Fraser", "Province"
+  )
   sites_first <- data.frame(Basin = basins_all, Station_ID_used = 2)
   sites_first[sites_first$Basin == "UpperFraserWest", ][2] <- paste(c("1A12", "1A16", "1A23", "1A12P"), collapse = ";")
   sites_first[sites_first$Basin == "UpperFraserEast", ][2] <- paste(c("1A02P", "1A14P", "1A17P", "1A05", "1A11", "1A15", "1A10", "1A06A", "1A05P", "1A19P", "1A03P"), collapse = ";")
@@ -72,7 +73,7 @@ basin_name <- function(id, exceptions = NULL) {
   sites_first[sites_first$Basin == "HaidaGwaii", ][2] <- paste(NA, collapse = ";")
 
   # basin not on the map currenly - future
-  sites_first[sites_first$Basin == "Nicola_old", ][2] <- paste(c("1C01",	"1C09",	"1C19",	"1C25",	"1C29",	"2F13",	"2F18",	"2F23",	"2F24"), collapse = ";")
+  sites_first[sites_first$Basin == "Nicola_old", ][2] <- paste(c("1C01", "1C09", "1C19", "1C25", "1C29", "2F13", "2F18", "2F23", "2F24"), collapse = ";")
   sites_first[sites_first$Basin == "FraserPlateau", ][2] <- paste(c("1C08", "1C22", "1C21"), collapse = ";")
   sites_first[sites_first$Basin == "LillBridge", ][2] <- paste(c("1C06", "1C39", "1C38P", "1C38", "1C40P", "1C40", "1C12P", "1C14P", "1C14", "1C37", "1C05P", "1C05", "1C18P", "1C28"), collapse = ";")
   sites_first[sites_first$Basin == "Quesnel", ][2] <- paste(c("1C33A", "1C13A", "1C17", "1C20P", "1C23", "1C41P"), collapse = ";")
@@ -83,11 +84,11 @@ basin_name <- function(id, exceptions = NULL) {
   if (id == "All") {
     return(sites_first)
   } else {
-   # find the basin within the dataframe and return the basin name
-   basin_site <- sites_first[sites_first$Station_ID_used %like% id, ] %>%
-     dplyr::select(Basin) %>%
-     dplyr::distinct(Basin)
-   return(basin_site)
+    # find the basin within the dataframe and return the basin name
+    basin_site <- sites_first[sites_first$Station_ID_used %like% id, ] %>%
+      dplyr::select(Basin) %>%
+      dplyr::distinct(Basin)
+    return(basin_site)
   }
 }
 
@@ -99,7 +100,7 @@ round_df <- function(x, digits) {
   # x: data frame
   # digits: number of digits to round
   numeric_columns <- sapply(x, is.numeric)
-  x[numeric_columns] <-  round(x[numeric_columns], digits)
+  x[numeric_columns] <- round(x[numeric_columns], digits)
   x
 }
 
@@ -108,8 +109,10 @@ round_df <- function(x, digits) {
 # =====================
 
 GET_client <- function(link) {
-  res <- httr::GET(link,
-                   httr::progress(), httr::user_agent("bcsnow"))
+  res <- httr::GET(
+    link,
+    httr::progress(), httr::user_agent("bcsnow")
+  )
   httr::stop_for_status(res)
   return(res)
 }
@@ -119,14 +122,16 @@ databc_GET_client <- function(layer, crs) {
 
   srs <- paste0("epsg:", crs)
   res <- httr::GET(base_url,
-                   query = list(service = "WFS",
-                                version = "2.0.0",
-                                request = "GetFeature",
-                                typeName = layer,
-                                outputFormat = "json",
-                                SRSNAME = srs))
+    query = list(
+      service = "WFS",
+      version = "2.0.0",
+      request = "GetFeature",
+      typeName = layer,
+      outputFormat = "json",
+      SRSNAME = srs
+    )
+  )
   httr::stop_for_status(res)
 
   sf::read_sf(httr::content(res, as = "text"))
-
 }
