311ac3539f7665cc31ac7d5d5d6cc4fbe4e55bb6
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-01-21 14:59:10 -0800

    linted file. Also added ability to screen out stations without any data within Data BC

--- R/hourly_archive_function.R
+++ R/hourly_archive_function.R
@@ -30,94 +30,158 @@ hourly_archive <- function(parameter = c("swe", "snow_depth", "precipitation", "
   }
   
   # Knit the current year with past year data if you need both current and archived data
-  if (any(yr %in% wtr_yr(Sys.Date())) ) {
+  if (any(yr %in% wtr_yr(Sys.Date()))) {
 
     if (parameter == "swe") {
   
       # knit the hourly swe archive with the current year hourly data
-      data <- bcdata::bcdc_get_data('5e7acd31-b242-4f09-8a64-000af872d68f', resource = '6789d794-c40a-4023-ac0b-0acc10d0d50f') %>%
-        dplyr::select(contains(c(id, "DATE(UTC)")))%>%
-        dplyr::full_join(bcdata::bcdc_get_data(record = '3a34bdd1-61b2-4687-8b55-c5db5e13ff50', resource = 'fe591e21-7ffd-45f4-b3b3-2291e4a6de15') %>%
+      data <- bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "6789d794-c40a-4023-ac0b-0acc10d0d50f") %>%
+        dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
+        dplyr::full_join(bcdata::bcdc_get_data(record = "3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "fe591e21-7ffd-45f4-b3b3-2291e4a6de15") %>%
                            dplyr::select(contains(c(id, "DATE(UTC)")))) %>%
         dplyr::mutate(parameter = "swe", "id" = id) %>%
-        dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-        dplyr::filter(!is.na(value))
+        dplyr::rename(value = contains(id), date_utc = "DATE(UTC)")
+        
+        if ("value" %in% colnames(data)) {
+          data <- data %>%
+            dplyr::filter(!is.na(value))
+        } else (
+          data <- data %>%
+            dplyr::mutate(value = NA) %>%
+            dplyr::filter(!is.na(value))
+        )
   
     } else if (parameter == "snow_depth") {
   
-    # knit the daily snow depth available pre 2003 with hourly 2003-current
-      data <-  bcdata::bcdc_get_data('5e7acd31-b242-4f09-8a64-000af872d68f', resource = '204f91d4-b136-41d2-98b3-125ecefd6887') %>%
+      # knit the daily snow depth available pre 2003 with hourly 2003-current
+      data <-  bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "204f91d4-b136-41d2-98b3-125ecefd6887") %>%
         dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
         # get current year sd
-        dplyr::full_join(bcdata::bcdc_get_data('3a34bdd1-61b2-4687-8b55-c5db5e13ff50', resource = 'abba1811-dd9a-4447-a297-2b5f81410abd') %>%
+        dplyr::full_join(bcdata::bcdc_get_data("3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "abba1811-dd9a-4447-a297-2b5f81410abd") %>%
                          dplyr::select(contains(c(id, "DATE(UTC)")))) %>%
         dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
         dplyr::mutate(parameter = "snow_depth", "id" = id) %>%
-        dplyr::rename(value = contains(id)) %>%
-        dplyr::filter(!is.na(value))
+        dplyr::rename(value = contains(id)) 
+      
+      if ("value" %in% colnames(data)) {
+        data <- data %>%
+          dplyr::filter(!is.na(value))
+      } else (
+        data <- data %>%
+          dplyr::mutate(value = NA) %>%
+          dplyr::filter(!is.na(value))
+      )
   
     } else if (parameter == "precipitation") {
   
       # knit the daily snow depth available pre 2003 with hourly 2003-current
-      data <-  bcdata::bcdc_get_data('5e7acd31-b242-4f09-8a64-000af872d68f', resource = '371a0479-1c6a-4f15-a456-11d778824f38') %>%
+      data <-  bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "371a0479-1c6a-4f15-a456-11d778824f38") %>%
         dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
         # get current year precip
-        dplyr::full_join(bcdata::bcdc_get_data(record = '3a34bdd1-61b2-4687-8b55-c5db5e13ff50', resource = '9f048a78-d74c-40c1-aa1f-9e2fcd1a19dd') %>%
-                         dplyr::select(contains(c(id, "DATE(UTC)"))) ) %>%
+        dplyr::full_join(bcdata::bcdc_get_data(record = "3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "9f048a78-d74c-40c1-aa1f-9e2fcd1a19dd") %>%
+                         dplyr::select(contains(c(id, "DATE(UTC)")))) %>%
         dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-        dplyr::mutate(parameter = "accum_precip", "id" = id) %>%
-        dplyr::filter(!is.na(value))
+        dplyr::mutate(parameter = "accum_precip", "id" = id) 
+      
+      if ("value" %in% colnames(data)) {
+        data <- data %>%
+          dplyr::filter(!is.na(value))
+      } else (
+        data <- data %>%
+          dplyr::mutate(value = NA) %>%
+          dplyr::filter(!is.na(value))
+      )
   
     } else if (parameter == "temperature") {
   
       # knit the daily snow depth available pre 2003 with hourly 2003-current
-      data <-  bcdata::bcdc_get_data('5e7acd31-b242-4f09-8a64-000af872d68f', resource = 'fba88311-34b9-4422-b5ae-572fd23b2a00') %>%
+      data <-  bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "fba88311-34b9-4422-b5ae-572fd23b2a00") %>%
         dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
         # get current year temperature
-        dplyr::full_join(bcdata::bcdc_get_data(record = '3a34bdd1-61b2-4687-8b55-c5db5e13ff50', resource = '9f048a78-d74c-40c1-aa1f-9e2fcd1a19dd') %>%
+        dplyr::full_join(bcdata::bcdc_get_data(record = "3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "9f048a78-d74c-40c1-aa1f-9e2fcd1a19dd") %>%
                          dplyr::select(contains(c(id, "DATE(UTC)")))) %>%
         dplyr::mutate(parameter = "temperature", "id" = id) %>%
-        dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-        dplyr::filter(!is.na(value)) 
+        dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") 
+      
+      if ("value" %in% colnames(data)) {
+        data <- data %>%
+          dplyr::filter(!is.na(value))
+      } else (
+        data <- data %>%
+          dplyr::mutate(value = NA) %>%
+          dplyr::filter(!is.na(value))
+      )
     }
   } else {
     
     if (parameter == "swe") {
       
       # get only the archived data
-      data <- bcdata::bcdc_get_data('5e7acd31-b242-4f09-8a64-000af872d68f', resource = '6789d794-c40a-4023-ac0b-0acc10d0d50f') %>%
-        dplyr::select(contains(c(id, "DATE(UTC)")))%>%
+      data <- bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "6789d794-c40a-4023-ac0b-0acc10d0d50f") %>%
+        dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
         dplyr::mutate(parameter = "swe", "id" = id) %>%
-        dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-        dplyr::filter(!is.na(value))
+        dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") 
+      
+      if ("value" %in% colnames(data)) {
+        data <- data %>%
+          dplyr::filter(!is.na(value))
+      } else (
+        data <- data %>%
+          dplyr::mutate(value = NA) %>%
+          dplyr::filter(!is.na(value))
+      )
       
     } else if (parameter == "snow_depth") {
       
       # knit the daily snow depth available pre 2003 with hourly 2003-current
-      data <-  bcdata::bcdc_get_data('5e7acd31-b242-4f09-8a64-000af872d68f', resource = '204f91d4-b136-41d2-98b3-125ecefd6887') %>%
+      data <-  bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "204f91d4-b136-41d2-98b3-125ecefd6887") %>%
         dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
         dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
         dplyr::mutate(parameter = "snow_depth", "id" = id) %>%
-        dplyr::rename(value = contains(id)) %>%
-        dplyr::filter(!is.na(value)) 
+        dplyr::rename(value = contains(id)) 
+      
+      if ("value" %in% colnames(data)) {
+        data <- data %>%
+          dplyr::filter(!is.na(value))
+      } else (
+        data <- data %>%
+          dplyr::mutate(value = NA) %>%
+          dplyr::filter(!is.na(value))
+      )
       
     } else if (parameter == "precipitation") {
       
       # knit the daily snow depth available pre 2003 with hourly 2003-current
-      data <-  bcdata::bcdc_get_data('5e7acd31-b242-4f09-8a64-000af872d68f', resource = '371a0479-1c6a-4f15-a456-11d778824f38') %>%
+      data <-  bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "371a0479-1c6a-4f15-a456-11d778824f38") %>%
         dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
         dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-        dplyr::mutate(parameter = "accum_precip", "id" = id) %>%
-        dplyr::filter(!is.na(value)) 
+        dplyr::mutate(parameter = "accum_precip", "id" = id)
+      
+      if ("value" %in% colnames(data)) {
+        data <- data %>%
+          dplyr::filter(!is.na(value))
+      } else (
+        data <- data %>%
+          dplyr::mutate(value = NA) %>%
+          dplyr::filter(!is.na(value))
+      )
       
     } else if (parameter == "temperature") {
       
       # knit the daily snow depth available pre 2003 with hourly 2003-current
-      data <-  bcdata::bcdc_get_data('5e7acd31-b242-4f09-8a64-000af872d68f', resource = 'fba88311-34b9-4422-b5ae-572fd23b2a00') %>%
+      data <-  bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "fba88311-34b9-4422-b5ae-572fd23b2a00") %>%
         dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
         dplyr::mutate(parameter = "temperature", "id" = id) %>%
-        dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-        dplyr::filter(!is.na(value)) 
+        dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") 
+      
+      if ("value" %in% colnames(data)) {
+        data <- data %>%
+          dplyr::filter(!is.na(value))
+      } else (
+        data <- data %>%
+          dplyr::mutate(value = NA) %>%
+          dplyr::filter(!is.na(value))
+      )
     }
   }
   
