142bc2fddcb1d3b1519a40de9ec9807546c3baa5
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-01-24 10:42:57 -0800

    fixed data retrieval for daily archive function

--- R/daily_archive_function.R
+++ R/daily_archive_function.R
@@ -24,7 +24,7 @@
 daily_archive <- function(parameter = c("swe", "snow_depth", "precipitation", "temperature"), get_year = "All", id) {
   yr <- get_year
   # Knit the current year with past year data if you need both current and archived data
-  if (any(get_year %in% c("all", "All", "ALL")) | any(yr %in% wtr_yr(Sys.Date()))) {
+  if (any(yr %in% c("all", "All", "ALL")) | any(yr %in% wtr_yr(Sys.Date()))) {
     if (parameter == "swe") {
       # knit the daily swe archive with daily SWE for this water year
       data <- bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "666b7263-6111-488c-89aa-7480031f74cd") %>%
@@ -44,24 +44,30 @@ daily_archive <- function(parameter = c("swe", "snow_depth", "precipitation", "t
       )
       
     } else if (parameter == "snow_depth") {
-      # historic daily data - not until present
-      historic_daily <- bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", 
-                                              resource = "945c144a-d094-4a20-a3c6-9fe74cad368a") %>%
-        dplyr::filter(variable == "Snow_Depth", Pillow_ID %in% id) %>%
+      
+      # Get snow depth from historic daily data - not always complete to present water year
+      historic_daily <- bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "945c144a-d094-4a20-a3c6-9fe74cad368a") %>%
+        dplyr::filter(variable == "SD", Pillow_ID %in% paste0("_", id)) %>%
         dplyr::rename(id = "Pillow_ID",  date_utc = "Date") %>%
-        dplyr::mutate(parameter = "snow_depth") %>%
-        dplyr::select(-code, -variable)
+        dplyr::mutate(parameter = "snow_depth", id = stringr::str_replace(id, "_", "")) %>%
+        dplyr::select(-code, -variable) 
       
       # knit the daily snow depth available pre 2003 with hourly 2003-current
-      data <-  bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", 
-                                     resource = "204f91d4-b136-41d2-98b3-125ecefd6887") %>%
+      data <-  bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "204f91d4-b136-41d2-98b3-125ecefd6887") %>%
         dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-        # get current year sd
-        dplyr::full_join(bcdata::bcdc_get_data("3a34bdd1-61b2-4687-8b55-c5db5e13ff50", 
-                                               resource = "abba1811-dd9a-4447-a297-2b5f81410abd") %>%
-                           dplyr::select(contains(c(id, "DATE(UTC)")))) %>%
+        dplyr::mutate(date = as.Date(`DATE(UTC)`)) %>%
         dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-        dplyr::mutate(parameter = "snow_depth", "id" = id) 
+        dplyr::mutate(date = as.Date(date_utc), parameter = "snow_depth", "id" = id) %>%
+        dplyr::group_by(date, parameter, id) %>%
+        dplyr::summarise(value = mean(value, na.rm = TRUE)) %>%
+        # cut out the data that is available within daily archive and knit together
+        dplyr::rename(date_utc = "date") %>%
+        dplyr::filter(date_utc > max(historic_daily$date_utc)) %>%
+        dplyr::full_join(historic_daily) %>%
+        dplyr::arrange(date_utc) %>%
+        # get current year sd
+        dplyr::full_join(daily_current(parameter = parameter, id = id) %>%
+                           dplyr::rename(date_utc = "date"))
       
       if ("value" %in% colnames(data)) {
         data <- data %>%
@@ -74,14 +80,19 @@ daily_archive <- function(parameter = c("swe", "snow_depth", "precipitation", "t
       
     } else if (parameter == "precipitation") {
       
-      # knit the daily snow depth available pre 2003 with hourly 2003-current
+      # knit the precipitation available until 2003 to the current year data.
+      # Note that precip data is only hourly from the data catalog.
+      # ************* WILL NEED TO CHANGE UTC BEFORE TAKING DAILY MEAN********************
       data <-  bcdata::bcdc_get_data("5e7acd31-b242-4f09-8a64-000af872d68f", resource = "371a0479-1c6a-4f15-a456-11d778824f38") %>%
         dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-        # get current year precip
-        dplyr::full_join(bcdata::bcdc_get_data(record = "3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "9f048a78-d74c-40c1-aa1f-9e2fcd1a19dd") %>%
-                           dplyr::select(contains(c(id, "DATE(UTC)")))) %>%
+        dplyr::mutate(parameter = "cum_precip", "id" = id) %>%
         dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-        dplyr::mutate(parameter = "accum_precip", "id" = id) 
+        dplyr::mutate(date = as.Date(date_utc)) %>%
+        dplyr::group_by(date, parameter, id) %>%
+        dplyr::summarise(value = mean(value, na.rm = TRUE)) %>%
+        # join with current year daily mean precip
+        dplyr::full_join(daily_current(parameter = parameter, id = id)) %>%
+        dplyr::rename(date_utc = "date") 
       
       if ("value" %in% colnames(data)) {
         data <- data %>%
