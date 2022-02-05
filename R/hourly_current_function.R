0e00d92ddc95288a2cabd07a591481cd7dd1bb95
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-02-04 16:43:24 -0800

    Changed code so that multiple station data can be retrieved.

--- R/hourly_current_function.R
+++ R/hourly_current_function.R
@@ -21,25 +21,67 @@
 #' @examples \dontrun{}
 
 hourly_current <- function(parameter = c("swe", "snow_depth", "precipitation", "temperature"), id) {
+  
   if (parameter == "swe") {
     current <- bcdata::bcdc_get_data("3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "fe591e21-7ffd-45f4-b3b3-2291e4a6de15") %>%
-      dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-      dplyr::mutate(parameter = "swe", "id" = id) %>%
-      dplyr::rename(value = contains(id), date_utc = "DATE(UTC)")
+      dplyr::select(contains(c(id, "DATE(UTC)"))) 
+    
+    colnames(current) <- substring(colnames(current), 1, 5)
+    
+    # Needs to be a dataframe to melt
+    current <- data.frame(current) 
+    colnames(current) <- substring(colnames(current), 2, 6)
+    
+    current_out <- current %>%
+      reshape::melt(id = "ATE.") %>%
+      dplyr::mutate(parameter = parameter) %>%
+      dplyr::rename(date_utc = "ATE.", id = "variable") 
+    
   } else if (parameter == "snow_depth") {
     current <- bcdata::bcdc_get_data("3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "abba1811-dd9a-4447-a297-2b5f81410abd") %>%
-      dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-      dplyr::mutate(parameter = "snow_depth", "id" = id) %>%
-      dplyr::rename(value = contains(id), date_utc = "DATE(UTC)")
+      dplyr::select(contains(c(id, "DATE(UTC)"))) 
+      
+    colnames(current) <- substring(colnames(current), 1, 5)
+      
+    # Needs to be a dataframe to melt
+    current <- data.frame(current) 
+    colnames(current) <- substring(colnames(current), 2, 6)
+      
+    current_out <- current %>%
+      reshape::melt(id = "ATE.") %>%
+      dplyr::mutate(parameter = parameter) %>%
+      dplyr::rename(date_utc = "ATE.", id = "variable") 
+      
   } else if (parameter == "precipitation") {
+    
     current <- bcdata::bcdc_get_data(record = "3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "9f048a78-d74c-40c1-aa1f-9e2fcd1a19dd") %>%
-      dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-      dplyr::mutate(parameter = "cum_precip", "id" = id) %>%
-      dplyr::rename(value = contains(id), date_utc = "DATE(UTC)")
+      dplyr::select(contains(c(id, "DATE(UTC)"))) 
+    
+    colnames(current) <- substring(colnames(current), 1, 5)
+    
+    # Needs to be a dataframe to melt
+    current <- data.frame(current) 
+    colnames(current) <- substring(colnames(current), 2, 6)
+    
+    current_out <- current %>%
+      reshape::melt(id = "ATE.") %>%
+      dplyr::mutate(parameter = "cum_precip") %>%
+      dplyr::rename(date_utc = "ATE.", id = "variable") 
+    
   } else if (parameter == "temperature") {
     current <- bcdata::bcdc_get_data(record = "3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "0bc026a2-7487-4f01-8b97-16d1b591a82f") %>%
-      dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-      dplyr::mutate(parameter = "temperature", "id" = id) %>%
-      dplyr::rename(value = contains(id), date_utc = "DATE(UTC)")
+      dplyr::select(contains(c(id, "DATE(UTC)"))) 
+    
+    colnames(current) <- substring(colnames(current), 1, 5)
+    
+    # Needs to be a dataframe to melt
+    current <- data.frame(current) 
+    colnames(current) <- substring(colnames(current), 2, 6)
+    
+    current_out <- current %>%
+      reshape::melt(id = "ATE.") %>%
+      dplyr::mutate(parameter = parameter) %>%
+      dplyr::rename(date_utc = "ATE.", id = "variable") 
   }
+  current_out
 }
