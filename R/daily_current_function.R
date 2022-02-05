cdaf038c57acad5c9f94509af423d1ba3961e0ef
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-02-04 16:43:44 -0800

    Changed code so that multiple station data can be retrieved.

--- R/daily_current_function.R
+++ R/daily_current_function.R
@@ -23,62 +23,104 @@
 
 daily_current <- function(parameter = c("swe", "snow_depth", "precipitation", "temperature"), id) {
   if (parameter == "swe") {
-    current <- bcdata::bcdc_get_data("3a34bdd1-61b2-4687-8b55-c5db5e13ff50", 
-                                     resource = "fe591e21-7ffd-45f4-b3b3-2291e4a6de15") %>%
-      dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-      dplyr::mutate(parameter = parameter, "id" = id) %>%
-      dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
+    current <- bcdata::bcdc_get_data("3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "fe591e21-7ffd-45f4-b3b3-2291e4a6de15") %>%
+      dplyr::select(contains(c(id, "DATE(UTC)"))) 
+    
+    colnames(current) <- substring(colnames(current), 1, 5)
+    
+    # Needs to be a dataframe to melt
+    current <- data.frame(current) 
+    colnames(current) <- substring(colnames(current), 2, 6)
+    
+    current_out <- current %>%
+      reshape::melt(id = "ATE.") %>%
+      dplyr::mutate(parameter = parameter) %>%
+      dplyr::rename(date_utc = "ATE.", id = "variable") %>%
+      # Get the 16:00 UTC measurement. Should also linear interpolate?
       dplyr::mutate(hour = lubridate::hour(date_utc)) %>%
       dplyr::filter(hour == 16) %>% # should eventually do interpolation in case 16:00 UTC measurement is missing??
       dplyr::select(-hour)
     
   } else if (parameter == "snow_depth") {
     current <- bcdata::bcdc_get_data("3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "abba1811-dd9a-4447-a297-2b5f81410abd") %>%
-      dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-      dplyr::mutate(parameter = parameter, "id" = id) %>%
-      dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-      # Get the daily mean *WILL NEED TO CHANGE
-      dplyr::mutate(date = as.Date(date_utc))
+      dplyr::select(contains(c(id, "DATE(UTC)"))) 
+    colnames(current) <- substring(colnames(current), 1, 5)
+    
+    # Needs to be a dataframe to melt
+    current <- data.frame(current) 
+    colnames(current) <- substring(colnames(current), 2, 6)
+    
+    current_m <- current %>%
+      #tibble::as_tibble()
+      reshape::melt(id = "ATE.") %>%
+      dplyr::mutate(parameter = parameter) %>%
+      dplyr::rename(date_utc = "ATE.", id = "variable")
       
-    if ("value" %in% colnames(current)) {
-      current <- current %>%
-        dplyr::group_by(date, parameter, id) %>%
-        dplyr::summarise(value = mean(value, na.rm = TRUE))%>%
+    # Get the daily mean snow depth
+    if ("value" %in% colnames(current_m)) {
+      current_out <- current_m %>%
+        dplyr::mutate(date = as.Date(date_utc)) %>%
+        dplyr::group_by(id, date) %>%
+        dplyr::summarise(value = mean(value, na.rm = TRUE)) %>%
         dplyr::rename(date_utc = date)
     } else {
-      current <- current
+      current_out <- current_m
     }
     
   } else if (parameter == "precipitation") {
     current <- bcdata::bcdc_get_data(record = "3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "9f048a78-d74c-40c1-aa1f-9e2fcd1a19dd") %>%
-      dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-      dplyr::mutate(parameter = "cum_precip", "id" = id) %>%
-      dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-      # Get the daily mean *WILL NEED TO CHANGE
-      dplyr::mutate(date = as.Date(date_utc)) %>%
-      dplyr::group_by(date, parameter, id) 
+      dplyr::select(contains(c(id, "DATE(UTC)"))) 
     
-    if ("value" %in% colnames(current)) {
-      current <- current %>%
-        dplyr::group_by(date, parameter, id) %>%
+    colnames(current) <- substring(colnames(current), 1, 5)
+    
+    # Needs to be a dataframe to melt
+    current <- data.frame(current) 
+    colnames(current) <- substring(colnames(current), 2, 6)
+    
+    current_m <- current %>%
+      reshape::melt(id = "ATE.") %>%
+      dplyr::mutate(parameter = "cum_precip") %>%
+      dplyr::rename(date_utc = "ATE.", id = "variable")
+    
+    if ("value" %in% colnames(current_m)) {
+      current_out <- current_m %>%
+        dplyr::mutate(date = as.Date(date_utc)) %>%
+        dplyr::group_by(id, date) %>%
         dplyr::summarise(value = mean(value, na.rm = TRUE))%>%
-        dplyr::rename(date_utc = date)
+        dplyr::rename(date_utc = date) %>%
+        dplyr::mutate(parameter = "cum_precip") 
     } else {
-      current <- current
+      current_out <- current_m
     }
+    
   } else if (parameter == "temperature") {
     current <- bcdata::bcdc_get_data(record = "3a34bdd1-61b2-4687-8b55-c5db5e13ff50", resource = "0bc026a2-7487-4f01-8b97-16d1b591a82f") %>%
-      dplyr::select(contains(c(id, "DATE(UTC)"))) %>%
-      dplyr::mutate("id" = id) %>%
-      dplyr::rename(value = contains(id), date_utc = "DATE(UTC)") %>%
-      # Get the daily max and min temp
-      dplyr::mutate(date = as.Date(date_utc)) %>%
-      dplyr::group_by(date, id) %>%
-      dplyr::summarise(t_max = max(value, na.rm = TRUE),
-                       t_min = min(value, na.rm = TRUE)) %>%
-      dplyr::rename(date_utc = date) %>%
-      reshape2::melt(id = c("date_utc", "id")) %>%
-      dplyr::rename(parameter = "variable") %>%
-      dplyr::arrange("date_utc")
+      dplyr::select(contains(c(id, "DATE(UTC)"))) 
+    
+    colnames(current) <- substring(colnames(current), 1, 5)
+    
+    # Needs to be a dataframe to melt
+    current <- data.frame(current) 
+    colnames(current) <- substring(colnames(current), 2, 6)
+    
+    current_m <- current %>%
+      reshape2::melt(id = "ATE.") %>%
+      dplyr::mutate(parameter = parameter) %>%
+      dplyr::rename(date_utc = "ATE.", id = "variable") 
+    
+    if ("value" %in% colnames(current_m)) {
+      current_out <- current_m %>%
+        # Get the daily max and min temp
+        dplyr::mutate(date = as.Date(date_utc)) %>%
+        dplyr::group_by(id, date) %>%
+        dplyr::summarise(t_max = max(value, na.rm = TRUE),
+                         t_min = min(value, na.rm = TRUE)) %>%
+        dplyr::rename(date_utc = date) %>%
+        reshape2::melt(id = c("date_utc", "id")) %>%
+        dplyr::rename(parameter = "variable") %>%
+        dplyr::arrange(date_utc, id, parameter)
+    } else {
+      current_out <- current_m
+    }
   }
 }
